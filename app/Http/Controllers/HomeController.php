78f6c94ac5ac25a9fdfa5b0f3f2086aa85c3f332
unknown <joudhabyi@gmail.com>
2025-03-28 23:52:00 +0200

    created models

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Car;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
-    {
+    {   // select all cars 
+        $cars = Car::get();
+        // select cars with condition
+        $cars = Car::where('published_at', '!=', null)->get();
+        // select car with specific id
+        $cars = Car::find(2);
+
+
+        dump($cars);
         return view('home.index');
     }
 }
